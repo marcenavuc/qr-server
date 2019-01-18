6906e0092cf1ebc7c3e33fe6b3f0da0c8cd91b99
marcenavuc <markenavuk@bk.ru>
2019-01-18 18:24:25 +0500

    make it better

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hellow'
+    return 'Hellow,this should be a table with'
 
 @app.route('/json-example', methods=['POST'])
 def json_example():
+    print(request.form)
     req_data = request.form
     print(req_data)
-    name = req_data['name']
-    return '''The name value is: {}'''.format(name)
+    name = req_data['id']
+    return ''.format(name)
+
+@app.route('/wake-up')
+def wake_up():
+    return 'OK'
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
